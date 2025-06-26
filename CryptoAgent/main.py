60b98c591c68d4e2859dbe9edf8c9eee778bc3bc
abdul565656 <abdullahkashif12b3@gmail.com>
2025-06-26 20:08:15 +0500

    --fix: deployment issues

--- CryptoAgent/main.py
+++ CryptoAgent/main.py
@@ -57,4 +57,4 @@ def index():
     return render_template("index.html", response=response)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
